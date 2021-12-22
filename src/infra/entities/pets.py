d3d4bfcedd78ff2e8529e64c294766596eed36ab
Wellington Ferreira <wellingtonf.souza@outlook.com>
2021-12-21 21:01:41 -0300

    feat: implementa arquitetura inicial com infra, config, entities, repo

---
+++ src/infra/entities/pets.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Enum
+import enum
+from src.infra.config import Base
+
+
+class AnimalTypes(enum.Enum):
+    """Animals types"""
+
+    DOG = "dog"
+    CAT = "cat"
+    FISH = "fish"
+    TURTLE = "turtle"
+
+
+class Pets(Base):
+    """Pets Entity"""
+
+    __tablename__ = "pets"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False, unique=True)
+    specie = Column(Enum(AnimalTypes), nullable=False)
+    age = Column(Integer)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    def __repr__(self) -> str:
+        return f"Pet [name={self.name}, specie={self.specie}, user_id={self.user_id}]"
---
+++ src/infra/entities/users.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from src.infra.config import Base
+
+
+class Users(Base):
+    """Users Entity"""
+
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    id_pet = relationship("Pets")
+
+    def __repr__(self) -> str:
+        return f"User [name={self.name}]"
